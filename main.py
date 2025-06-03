35674648b5fec94bbcb93296c853f26a73cab76a
Bruce <sunghunet@gmail.com>
2025-06-04 03:56:05 +0900

    commit 1123

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
-from dotenv import load_env
-load_env()
+from dotenv import load_dotenv
+from langchain_mcp_adapters.tools import load_mcp_tools
+from langchain_openai import ChatOpenAI
+from langgraph.prebuilt import create_react_agent
+from mcp import ClientSession, StdioServerParameters
+from mcp.client.stdio import stdio_client
+
+load_dotenv()
+
+llm = ChatOpenAI(model="gpt-4o")
+
+stdio_server_params = StdioServerParameters(
+    command = "python",
+    args = ["D:\\vibe-coding\\mcp-crash-course-udemy\\langchain-mcp-adapter\\servers\\math_server.py"]
+)
 
 async def main():
     print("Hello from langchain-mcp-adapter!")
@@ -7,4 +20,3 @@ async def main():
 
 if __name__ == "__main__":
     asyncio.run(main())
- # 
